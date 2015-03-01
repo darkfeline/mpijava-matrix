54f34e7b34c73c3eb94d652fa29bf2b999c32088
Allen Li <darkfeline@felesatra.moe>
2015-02-28 20:10:52 -0500

    Fix int[] definition

--- Matrix.java
+++ Matrix.java
@@ -216,7 +216,7 @@ class Matrix {
      */
     static int[] deflate(int[][] m) {
         int size = m.length;
-        int[] result = size * size;
+        int[] result = new int[size * size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 result[i * size + j] = m[i][j];
