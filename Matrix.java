9dc34939615007da9d12771b1c36847770d7d29d
Allen Li <darkfeline@felesatra.moe>
2015-02-28 23:15:09 -0500

    Fix join

--- Matrix.java
+++ Matrix.java
@@ -157,8 +157,10 @@ class Matrix {
                     // Print line
                     StringBuilder sb = new StringBuilder();
                     for (int i = 0; i < buffer.length; i++) {
+                        if (i != 0) {
+                            sb.append(" ");
+                        }
                         sb.append(buffer[i]);
-                        sb.append(" ");
                     }
                     System.out.println(sb.toString());
                 }
